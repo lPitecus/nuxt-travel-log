b3b300950f01c89e58955882568e122d45d6ef01
Arthur Silva <143892566+lPitecus@users.noreply.github.com>
2025-09-07 18:53:29 -0300

    setup eslint and lint-staged (#48)

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+
+export default antfu({
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: [".pnpm-store/**"],
+}, {
+  rules: {
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
