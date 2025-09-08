1162ed94bdef407f06457331d8b57f036646dc8d
Arthur Silva <143892566+lPitecus@users.noreply.github.com>
2025-09-07 21:21:01 -0300

    add landing page (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
