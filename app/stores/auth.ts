df630f0d54da05e71ab80d31c9ce4cd98b2fb75e
Arthur Silva <143892566+lPitecus@users.noreply.github.com>
2025-09-10 00:59:12 -0300

    feat: add github oauth flow (#55)

---
+++ app/stores/auth.ts
@@ -0,0 +1,24 @@
+// Esse arquivo serve para sincronizar o estado dos dois botões de
+// login com o github
+
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+export const useAuthStore = defineStore("useAuthStore", () => {
+  const loading = ref(false);
+  async function signIn() {
+    loading.value = true;
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
+    });
+    loading.value = false;
+  }
+
+  return {
+    loading,
+    signIn,
+  };
+});
